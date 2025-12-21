a8ec5e263f7555d417f23d69c146b3e61fdd3987
Trang00001 <lemaitrang000@gmail.com>
2025-12-21 19:14:22 +0700

    Change BASE_URL port to 8080

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 
         debug {
 //            buildConfigField("String", "BASE_URL", "${properties["BASE_URL"]}")
-            buildConfigField("String", "BASE_URL", "\"http://10.0.2.2:8081/\"")
+            buildConfigField("String", "BASE_URL", "\"http://10.0.2.2:8080/\"")
         }
 
         release {
